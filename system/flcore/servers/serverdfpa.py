26e8fd33595b3a9a9e6fb95f22bca999470107d7
19256 <1925620345@qq.com>
2025-06-29 20:45:43 +0800

    base

--- system/flcore/servers/serverdfpa.py
+++ system/flcore/servers/serverdfpa.py
@@ -66,12 +66,12 @@ class FedDFPA(Server):
             if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
                 break
 
-        print("\nBest accuracy.")
-        # self.print_(max(self.rs_test_acc), max(
-        #     self.rs_train_acc), min(self.rs_train_loss))
-        print(max(self.rs_test_acc))
-        print(max(self.rs_test_acc2))
-        print(max(self.rs_test_acc3))
+            print("\nBest accuracy.")
+            # self.print_(max(self.rs_test_acc), max(
+            #     self.rs_train_acc), min(self.rs_train_loss))
+            print(max(self.rs_test_acc))
+            print(max(self.rs_test_acc2))
+            print(max(self.rs_test_acc3))
         print(sum(self.Budget[1:]) / len(self.Budget[1:]))
 
         self.save_results()
